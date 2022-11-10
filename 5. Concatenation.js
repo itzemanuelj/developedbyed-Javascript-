3074b4414f5d1de67dcc2971e1b9e39a033e5f8e
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-10 00:19:10 -0500

    added string cnccatenation.js

---
+++ 5. Concatenation.js
@@ -0,0 +1,17 @@
+const bank1 = 100;
+const bank2 = 50;
+
+allBanks = bank1 + bank2;
+
+console.log(allBanks);
+//150
+
+const user = 'Justin'
+const greeting = 'welcome to our website';
+
+
+console.log(greeting + ''+ user);
+
+const fname = 'justin';
+const greetings = `welcome to our websire + ${fname}`;
+
---
+++ 6. Comparisons and Conditions.js
@@ -0,0 +1,43 @@
+5 === 5;
+fname !== 'fname';
+
+if (1 > 2) {
+    console.log('message');
+}
+
+  function checkValue(a, b) {
+    if (a === 1)
+      if (b === 2)
+        console.log("a is 1 and b is 2");
+      else
+        console.log("a is not 1");
+  }
+
+let age = 18;
+
+if (age >= 65) {
+    console.log('you are a senior');
+}
+else if (age >= 18) {
+    console.log('you are an adult');
+}
+else if (age > 0) {
+    console.log('you havent been born yet');
+}
+else{
+    console.log('you are a child');
+}
+
+
+var online = true;
+
+if (online == true) {
+    console.log('bro is online');
+}
+else {
+    console.log('bro is offine');
+}
+
+let online = true ? 'online' : 'offlnie';
+
+let age2 = 18 >= 17 ? 'your an adult' : 'not old enough';
